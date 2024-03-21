e401af1d7596483249b485f5fdb98ccfc3b385df
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-03-21 16:35:57 -0400

    Created an instance of the discord bot and gave it intents, and called the bot instance through a login chain method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./style.css";
 
 const { Client, IntentsBitField } = require("discord.js");
+
+// create a bot instance
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers, // bot will have access to members inside the server
+    IntentsBitField.Flags.GuildMessages, // bot will access to messages inside the server
+    IntentsBitField.Flags.MessageContent, // bot will be able to read messages inside the server
+  ],
+});
+
+client.login(
+  "MTIyMDQ1NjExNjQyNTcyMzkzNA.GnBzbD.GeebcYnow3cYEnS24Jx18SXd9jbdUOwdn5lShg",
+);
