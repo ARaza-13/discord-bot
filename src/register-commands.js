a764677fb2a9d083d6517b8964fbea3b85eca046
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-03-23 20:17:27 -0400

    Defined and registered slash commands

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,2 +1,27 @@
-require("dotenv").config();
+require("dotenv").config(); // access bot and server ID from .env file
 const { REST, Routes } = require("discord.js");
+
+const commands = [
+  {
+    name: "hey",
+    description: "Replies with hey!",
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+// Register slash commands
+(async () => {
+  try {
+    console.log("Registering slash commands...");
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
+
+    console.log("Slash commands were registered successfully");
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
+})();
