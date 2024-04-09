add361a312df476c49cf704d90db2ad6655b50d0
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-04-09 14:20:41 -0400

    modified ping command to show websocket ping and the bot's ping based on the interaction response time

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -6,7 +6,17 @@ module.exports = {
   // options: Object[],
   // deleted: Boolean,
 
-  callback: (client, interaction) => {
-    interaction.reply(`Pong! ${client.ws.ping}ms`);
+  callback: async (client, interaction) => {
+    await interaction.deferReply();
+
+    // fetch the information from the defer reply
+    const reply = await interaction.fetchReply();
+
+    // compare the timestamp of the reply with the timestamp of the interaction to get the ping
+    const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+    interaction.editReply(
+      `Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`,
+    );
   },
 };
